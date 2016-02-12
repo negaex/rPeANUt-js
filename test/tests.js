4322b6334145f4faec78b273dc1886d4608edcdd
noiach <gh@noiach.com>
2016-02-13 09:50:24 +1100

    Testing Travis CI

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,8 @@
 QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
 });
+
+
+QUnit.test( "hello2 test", function( assert ) {
+  assert.ok( 2 == "2", "Passed!" );
+});
